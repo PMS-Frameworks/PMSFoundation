cc5ffa4c1d9f34d2ebdedf076604c161d58e0e5a
GoEun Jeong <gogo8272@gmail.com>
2021-06-19 21:43:29 +0900

    Modify SPM Source Path

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "PMSPresentation",
+    platforms: [.iOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PMSPresentation",
             targets: ["PMSPresentation"]),
@@ -26,13 +26,9 @@ let package = Package(
         .package(url: "https://github.com/PMS-Frameworks/PMSRxModule", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PMSPresentation",
-            dependencies: []),
-        .testTarget(
-            name: "PMSPresentationTests",
-            dependencies: ["PMSPresentation"]),
+            dependencies: [],
+            path: "PMSPresentation/Classes"))
     ]
 )
