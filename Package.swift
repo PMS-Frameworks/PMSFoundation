2b05ec40367986f98c44ae437cd770d76e5c5a07
GoEun Jeong <gogo8272@gmail.com>
2021-06-19 21:40:59 +0900

    Modify SPM url

--- Package.swift
+++ Package.swift
@@ -13,17 +13,17 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: 'https://github.com/RxSwiftCommunity/RxDataSources', from: "4.0.1"),
-        .package(url: 'https://github.com/SnapKit/SnapKit', from: "5.0.0"),
-        .package(url: 'https://github.com/devxoul/Then', from: "2.7.0"),
-        .package(url: 'https://github.com/Juanpe/SkeletonView', from: "1.17.0"),
-        .package(url: 'https://github.com/Swinject/Swinject', from: "2.7.0"),
-        .package(url: 'https://github.com/Swinject/SwinjectAutoregistration', from: "2.7.0"),
-        .package(url: 'https://github.com/airbnb/lottie-ios', from: "3.2.0"),
-        .package(url: 'https://github.com/onevcat/Kingfisher', from: "6.0.0"),
-        .package(url: 'https://github.com/ashleymills/Reachability.swift', from: "5.0.0"),
-        .package(url: 'https://github.com/RxSwiftCommunity/RxFlow', from: "2.11.0"),
-        .package(url: 'https://github.com/PMS-Frameworks/PMSRxModule', from: "1.0.0"),
+        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources", from: "4.0.1"),
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.0"),
+        .package(url: "https://github.com/devxoul/Then", from: "2.7.0"),
+        .package(url: "https://github.com/Juanpe/SkeletonView", from: "1.17.0"),
+        .package(url: "https://github.com/Swinject/Swinject", from: "2.7.0"),
+        .package(url: "https://github.com/Swinject/SwinjectAutoregistration", from: "2.7.0"),
+        .package(url: "https://github.com/airbnb/lottie-ios", from: "3.2.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher", from: "6.0.0"),
+        .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.0.0"),
+        .package(url: "https://github.com/RxSwiftCommunity/RxFlow", from: "2.11.0"),
+        .package(url: "https://github.com/PMS-Frameworks/PMSRxModule", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
